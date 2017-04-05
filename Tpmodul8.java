ea9f53467e2df2175fab4fb6cd75af46349344aa
acaikhsaniova <ikhsaniovaaca@gmail.com>
2017-04-05 09:36:16 +0700

    Update Tpmodul8.java

--- Tpmodul8.java
+++ Tpmodul8.java
@@ -17,7 +17,7 @@ public class Tpmodul8 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int a=1,b=1,hasil=1,c;
+        int a=4,b=1,hasil=4,c;
         Scanner input = new Scanner (System.in);
         System.out.print ("Masukan Deret Fibonaci  : ");
              c = input.nextInt ();
