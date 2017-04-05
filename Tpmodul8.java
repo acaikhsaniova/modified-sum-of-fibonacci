976c82d379eec8d04dba214869f1f64911e6a46a
tasyaikhsaniova <tasyaikh9@gmail.com>
2017-04-05 08:49:58 +0700

    Add files via upload

---
+++ Tpmodul8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpmodul8;
+import java.util.Scanner; 
+/**
+ *
+ * @author Praktikan
+ */
+public class Tpmodul8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a=1,b=1,hasil=1,c;
+        Scanner input = new Scanner (System.in);
+        System.out.print ("Masukan Deret Fibonacci  : ");
+             c = input.nextInt ();
+             for(int i=1; i<=c; i++){            
+         a=b;
+         b=hasil;
+         System.out.print(hasil+" ");
+         hasil=a+b;
+         }
+     }
+
+}
